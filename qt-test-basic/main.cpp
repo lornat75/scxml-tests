dbf361832b83f60995ef7b278a6ad221066d255d
lornat75 <lorenzo.natale@iit.it>
2024-03-22 15:41:35 +0100

    stress test glitch

--- qt-test-basic/main.cpp
+++ qt-test-basic/main.cpp
@@ -11,15 +11,33 @@
 
 void myFunction(MyStateMachine *machine)
 {
-  int count=10;
-  while(--count) {
-    std::cout<<"Thread is running...\n";
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+  int count=1000000;
+  while(count--) {
+   // count++;
+    //std::cout<<"Thread is running...\n";
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
     if (count%2==0)
-      machine->submitEvent("start");
+    {
+      //  std::cout<<"Submitting start...\n";
+        machine->submitEvent("start");
+     // machine->submitEvent("onTransition");
+    }
     else
-      machine->submitEvent("stop");
+    {
+       // std::cout<<"Submitting stop...\n";
+        machine->submitEvent("stop");
+    }
+
+  }
+
+  count=10;
+  while(count--)
+  {
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
   }
 
   QCoreApplication::quit();
@@ -30,8 +48,22 @@ int main(int argc, char *argv[])
   QCoreApplication app(argc, argv);
 
   MyStateMachine stateMachine;
+  
+  stateMachine.connectToEvent("onTransition", [](const QScxmlEvent &event){
+    /* std::cout << event.data().isNull() << std::endl;
+    std::cout << event.data().toMap()["state"].toString().toStdString() << std::endl; */
+    std::cout << "received message onTransition";
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    std::cout << "...done!"<< std::endl;
+  });
+
 
   stateMachine.start();
+
+  if (!stateMachine.isInitialized()) {
+    std::cerr << "State machine failed to initialize." << std::endl;
+    return -1;
+  }
 
   std::thread t1(myFunction, &stateMachine);
 
